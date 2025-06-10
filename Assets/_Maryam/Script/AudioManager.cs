33c059d3de52eee12ee110a94abfb90e4165af29
MaryamAlsuwaidi39 <mas.392001@outlook.com>
2025-06-10 14:59:08 +0400

    MA: Fighter Size

---
+++ Assets/_Maryam/Script/AudioManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager Instance;
+
+    public AudioSource musicSource;
+    public AudioClip titleTheme;
+    public AudioClip gameTheme;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Awake()
+    {
+        // Singleton pattern to keep the audio manager alive between scenes
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        PlayTitleTheme();
+    }
+
+    // Update is called once per frame
+
+    public void PlayTitleTheme()
+    {
+        musicSource.clip = titleTheme;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    public void PlayGameTheme()
+    {
+        musicSource.clip = gameTheme;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+}
