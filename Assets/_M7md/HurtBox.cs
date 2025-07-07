ee9005b3027658faa083b856ff41b77d05ca0e1a
Ammar Albreiki <ammar.s.albreiki@gmail.com>
2025-07-07 12:57:54 +0400

    Flash Sprite hopefully won't ruin animation

--- Assets/_M7md/HurtBox.cs
+++ Assets/_M7md/HurtBox.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class HurtBox : MonoBehaviour
 {
-    private SpriteRenderer playerRenderer;
-    private Material originalMaterial;
+    [SerializeField] GameObject otherSprite;
+    [SerializeField] SpriteRenderer playerRenderer;
+    [SerializeField] Material originalMaterial;
     [SerializeField] Material flashMaterial;
     private float shakeMagnitude;
     private float shakeTimer;
@@ -16,8 +17,6 @@ public class HurtBox : MonoBehaviour
 
     void Start()
     {
-        playerRenderer = GetComponentInParent<SpriteRenderer>();
-        originalMaterial = playerRenderer.material;
         shakeMagnitude = 0.05f; 
         lastHitBackground.enabled = true;
         Color alph = lastHitBackground.color;
@@ -28,70 +27,46 @@ public class HurtBox : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("HitBox")) return;
-
-       StartCoroutine(PauseAndShake2());
-    }
-
-    IEnumerator PauseAndShake()
-    {
-        originalCamTransform = Camera.transform;
-        playerRenderer.material = flashMaterial;
-        Time.timeScale = 0f;
-        if (gameManager.p1Score == 3 || gameManager.p2Score == 3)
-            OriginalshakeTimer = 2f;
-        else
-            OriginalshakeTimer = 0.5f;
-        shakeTimer = OriginalshakeTimer;
-        while (shakeTimer > 0f)
-        {
-            Vector2 shakeOffset = Random.insideUnitCircle * shakeMagnitude;
-            Camera.transform.localPosition = Camera.transform.localPosition + new Vector3(shakeOffset.x * 4, 0f, 0f);
-            if (gameManager.p1Score == 3 || gameManager.p2Score == 3)
-            {
-                Color alph = lastHitBackground.color;
-                alph.a = Mathf.Lerp(0, 1, shakeTimer);
-                Time.timeScale = Mathf.Lerp(1, 0, shakeTimer);
-                lastHitBackground.color = alph;
-            }
-            shakeTimer -= Time.unscaledDeltaTime;
-            Camera.transform.localPosition = originalCamTransform.localPosition;
-            yield return null;
-        }
-        shakeTimer = OriginalshakeTimer;
-        playerRenderer.material = originalMaterial;
-        Time.timeScale = 1f;
+        StartCoroutine(PauseAndShake2());
     }
     
     IEnumerator PauseAndShake2()
     {
         Vector3 originalCamPosition = Camera.transform.localPosition;
         Color originalColor = playerRenderer.color;
-        playerRenderer.color = Color.white;
-        // playerRenderer.material = flashMaterial;
-
+        otherSprite.SetActive(true);
+        playerRenderer.material = flashMaterial;
+        if (gameManager.p1Score == 3 || gameManager.p2Score == 3)
+        {
+            Color alph = lastHitBackground.color;
+            alph.a = 1;
+            lastHitBackground.color = alph;
+        }
         Time.timeScale = 0f;
         shakeTimer = (gameManager.p1Score == 3 || gameManager.p2Score == 3) ? 2f : 0.5f;
-
         while (shakeTimer > 0f)
         {
             Vector2 shakeOffset = Random.insideUnitCircle * shakeMagnitude;
             Camera.transform.localPosition = originalCamPosition + new Vector3(shakeOffset.x, 0f, 0f);
-
+            shakeTimer -= Time.unscaledDeltaTime;
+            yield return null;
+        }
+        shakeTimer = (gameManager.p1Score == 3 || gameManager.p2Score == 3) ? 2f : 0.5f;
+        while (shakeTimer > 0f)
+        {
             if (gameManager.p1Score == 3 || gameManager.p2Score == 3)
             {
                 Color alph = lastHitBackground.color;
                 alph.a = Mathf.Lerp(0, 1, shakeTimer);
-                Time.timeScale = Mathf.Lerp(1, 0, shakeTimer);
                 lastHitBackground.color = alph;
             }
-
+            Time.timeScale = Mathf.Lerp(1, 0, shakeTimer);
             shakeTimer -= Time.unscaledDeltaTime;
             yield return null;
         }
-
-        Camera.transform.localPosition = originalCamPosition;
-        playerRenderer.color = originalColor;
-        // playerRenderer.material = originalMaterial;
+        // Camera.transform.localPosition = originalCamPosition;
+        playerRenderer.material = originalMaterial;
+        otherSprite.SetActive(false);
         Time.timeScale = 1f;
     }
 
